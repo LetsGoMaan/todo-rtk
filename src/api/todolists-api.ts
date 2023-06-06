3ef3d41bd9edcd9fca8a699e05fce9d5222e31a1
LetsGoMan <ilikeseik@gmail.com>
2023-06-06 19:24:30 +0300

    1. Add Redux Toolkit, createSlice

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': '5387a064-53c2-41e2-a48f-3c0a2fb2fa92'
     }
 }
 const instance = axios.create({
